de7d6332164e9913e29fb8e330fee70140dcac1e
TrynaThinkOf1 <zeviberlin@gmail.com>
2024-11-11 12:56:10 -0500

    new setup

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="RpiL",  # Package name
-    version="0.1.0",  # Version
+    version="0.1.1",  # Version
     packages=find_packages(where="src"),  # Automatically find packages in 'src'
     package_dir={"": "src"},  # Tell setuptools where your package is
-    install_requires=["RPi.GPIO, time, threading"],  # List of dependencies
+    install_requires=["RPi.GPIO"],  # List of dependencies (only external packages)
     description="Library for controlling Raspberry Pi hardware. CAN ONLY BE INSTALLED ON RASPBERRY PI (due to Rpi.GPIO dependency).",
     author="Zevi Berlin",
     author_email="zeviberlin@gmail.com",
@@ -13,6 +13,5 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Raspbarian",
     ],
 )
