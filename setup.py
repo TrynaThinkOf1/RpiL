38a5991c6ecb90eab4a331dc3c5530b659b13312
TrynaThinkOf1 <zeviberlin@gmail.com>
2024-11-11 09:42:18 -0500

    changed setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     packages=find_packages(where="src"),  # Automatically find packages in 'src'
     package_dir={"": "src"},  # Tell setuptools where your package is
     install_requires=["RPi.GPIO"],  # List of dependencies
-    description="Library for controlling Raspberry Pi hardware.",
+    description="Library for controlling Raspberry Pi hardware. CAN ONLY BE INSTALLED ON RASPBERRY PI (due to Rpi.GPIO dependency).",
     author="Zevi Berlin",
     author_email="zeviberlin@gmail.com",
     license="MIT",  # Open source license
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Raspbarian",
     ],
 )
