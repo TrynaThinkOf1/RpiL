7bd5b2e3accd81ba059d4c65b69fb3d3933d2682
TrynaThinkOf1 <zeviberlin@gmail.com>
2024-11-12 16:07:16 -0500

    edited pwm

--- src/rpi_gpio/PWM.py
+++ src/rpi_gpio/PWM.py
@@ -5,33 +5,35 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 class PWM:
-    def __init__(self, pin, frequency=1000):
+    def __init__(self, pin, frequency=1000, duty_cycle=50):
         """This uses GPIO pin-number on the pi.\n
         This class is for Pulse Width Modulation"""
         self.pin = int(pin)
         self.frequency = frequency
+        self.duty_cycle = duty_cycle
 
         GPIO.setup(self.pin, GPIO.OUT)
         self.pwm = GPIO.PWM(self.pin, self.frequency)
         self.pwm_started = False
 
-    def start(self, duty_cycle=50):
+    def start(self):
         """Start the PWM with specified Duty-Cycle"""
         if not self.pwm_started:
-            self.pwm.start(duty_cycle)
+            self.pwm.start(self.duty_cycle)
             self.pwm_started = True
         else:
-            self.pwm.ChangeDutyCycle(duty_cycle)
+            self.pwm.ChangeDutyCycle(self.duty_cycle)
 
     def change_duty_cycle(self, duty_cycle):
         if self.pwm_started:
             self.pwm.ChangeDutyCycle(duty_cycle)
+        self.duty_cycle = duty_cycle
 
-    def change_frequency(self, frequency, duty_cycle=50):
+    def change_frequency(self, frequency):
         if self.pwm_started:
             self.stop()
             self.pwm = GPIO.PWM(self.pin, frequency)
-            self.pwm.start(duty_cycle)
+            self.pwm.start(self.duty_cycle)
             self.pwm_started = True
         self.frequency = frequency
 
