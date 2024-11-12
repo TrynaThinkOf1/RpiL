3d31766670b7acc6658c0445bf01704681c029c6
TrynaThinkOf1 <zeviberlin@gmail.com>
2024-11-11 21:52:10 -0500

    temp sensor

--- src/rpi_gpio/__init__.py
+++ src/rpi_gpio/__init__.py
@@ -6,6 +6,6 @@ from .buzzer import buzzer
 from .motor import motor
 from .pin import pin
 from .USDS import USDS
-from .Heat_Sensor import DHT11, DHT22, AM2302
+from .Temperature_Sensor import DHT11, DHT22, AM2302
 from .PIR import PIR
 from .IR import IR_Receiver, IR_LED
