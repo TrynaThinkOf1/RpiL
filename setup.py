23ced35057626a7786123a4a45b49df43557a058
TrynaThinkOf1 <zeviberlin@gmail.com>
2024-11-12 18:19:37 -0500

    cpu temp

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.5",  # Version
     packages=find_packages(where="src"),  # Automatically find packages in 'src'
     package_dir={"": "src"},  # Tell setuptools where your package is
-    install_requires=["RPi.GPIO", "Adafruit_DHT", "pigpio"],  # List of dependencies (only external packages)
+    install_requires=["RPi.GPIO", "Adafruit_DHT", "pigpio", "gpiozero"],  # List of dependencies (only external packages)
     description="Library for controlling Raspberry Pi hardware. CAN ONLY BE INSTALLED ON RASPBERRY PI (due to dependancies).",
     author="Zevi Berlin",
     author_email="zeviberlin@gmail.com",
