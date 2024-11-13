a992d02892c2eefa535b293a77463d7032eb5af8
TrynaThinkOf1 <zeviberlin@gmail.com>
2024-11-13 10:32:31 -0500

    fixed for optional dependancies

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 from setuptools import setup, find_packages
 
+# Define Raspberry Pi-specific dependencies
+raspberry_pi_deps = [
+    "RPi.GPIO",
+    "Adafruit_DHT"
+]
+
 setup(
-    name="RpiL",  # Package name
-    version="0.1.5",  # Version
-    packages=find_packages(where="src"),  # Automatically find packages in 'src'
-    package_dir={"": "src"},  # Tell setuptools where your package is
-    install_requires=["RPi.GPIO", "Adafruit_DHT", "pigpio", "gpiozero"],  # List of dependencies (only external packages)
-    description="Library for controlling Raspberry Pi hardware. CAN ONLY BE INSTALLED ON RASPBERRY PI (due to dependancies).",
+    name="RpiL",
+    version="0.1.6",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "pigpio",
+        "gpiozero"
+    ],
+    extras_require={
+        "rpi": raspberry_pi_deps  # Raspberry Pi-specific dependencies
+    },
+    description="Library for controlling Raspberry Pi hardware.",
     author="Zevi Berlin",
     author_email="zeviberlin@gmail.com",
-    license="MIT",  # Open source license
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
